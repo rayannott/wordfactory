f95032dccdbc25ca312ff5d53b77b0a7c427e734
rayannott <imjobjob@gmail.com>
2022-08-13 00:27:35 +0300

    created more exceptions (mostly for command handling)

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,38 @@
 class OccupiedContainer(Exception):
     pass
 
+
 class EmptyCell(Exception):
     pass
+
 
 class OutsideOfField(Exception):
     pass
 
+
 class UnknownCommand(Exception):
     pass
 
+
 class EmptyHand(Exception):
+    pass
+
+# ...
+
+
+class CommandException(Exception):
+    pass
+
+
+class UnmatchedParentheses(CommandException):
+    pass
+
+
+class IncorrectLoopSyntax(CommandException):
+    pass
+
+class IncorrectReferencesSyntax(CommandException):
+    pass
+
+class GroupInsideLoop(CommandException):
     pass
