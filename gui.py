f37bfd021ea81c8ca9c0d3a7f549de8346ea88a2
rayannott <imjobjob@gmail.com>
2022-08-13 20:00:52 +0300

    log textbox, win window

--- gui.py
+++ gui.py
@@ -1,27 +1,42 @@
+from copy import deepcopy
 import pygame
 import pygame_gui
-from pygame_gui.elements import UIButton, UIPanel, UITextEntryLine, UITextBox
+from pygame_gui.elements import UIButton, UIPanel, UITextEntryLine, UITextBox, UIWindow
+from pygame_gui.windows import UIMessageWindow
 
 from objects import Game
+from exceptions import *
 from utils import *
 
 
 class UICell(UIButton):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # self.disable()
-
+    def __init__(self, relative_rect, text, manager, OPTIONS, unit_id=None, group_id=None, **kwargs):
+        super().__init__(relative_rect, text, manager, **kwargs)
+        self.unit_id = unit_id
+        self.group_id = group_id
+        if OPTIONS['show_ids'] and self.unit_id is not None:
+            rect_unit_id = pygame.Rect(0, 0, 0, 0)
+            rect_unit_id.size = 30, 30
+            rect_unit_id.topleft = self.relative_rect.topleft
+            self.unit_id_textbox = UITextBox(str(self.unit_id), rect_unit_id, manager)
+        if self.group_id is not None:
+            rect_group_id = pygame.Rect(0, 0, 0, 0)
+            rect_group_id.size = 30, 30
+            rect_group_id.topright = self.relative_rect.topright
+            self.group_id_textbox = UITextBox(str(self.group_id), rect_group_id, manager)
+    
 
 class FieldPanel(UIPanel):
-    def __init__(self, field, manager, *args, **kwargs):
+    def __init__(self, manager, field, OPTIONS, **kwargs):
+        self.OPTIONS = OPTIONS
         self.manager = manager
         self.field = field
         field_panel_rect = pygame.Rect(
-            (198, 0), (1002, 670))
-        starting_layer_height = 0
-        super().__init__(field_panel_rect, starting_layer_height, manager, *args, **kwargs)
+            (198, 1), (1002, 670))
+        super().__init__(field_panel_rect, starting_layer_height=0, manager=manager, **kwargs)
         self.create_cells()
-    
+
+
     def update_field(self, field):
         self.field = field
         for i in range(8):
@@ -34,14 +49,18 @@ class FieldPanel(UIPanel):
         start_y = 0 + margin
         self.cells = [[UICell(relative_rect=pygame.Rect((start_x + j*(margin + CELL_SIZE[0]), start_y + i*(margin + CELL_SIZE[1])), CELL_SIZE),
                               text=str(self.field[i][j]),
-                              manager=self.manager) for j in range(12)] for i in range(8)]
+                              manager=self.manager,
+                              OPTIONS=self.OPTIONS,
+                              unit_id=None if self.field[i][j].contents is None else self.field[i][j].contents.id,
+                              group_id=None if self.field[i][j].contents is None else self.field[i][j].contents.IN_GROUP) for j in range(12)] for i in range(8)]
 
     def process_event(self, event: pygame.event.Event) -> bool:
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
             for i, cells_row in enumerate(self.cells):
                 for j, cell in enumerate(cells_row):
                     if event.ui_element == cell:
-                        print(i, j, self.field[i][j])
+                        print(i, j, self.field[i][j], type(
+                            self.field[i][j].contents))
         return super().process_event(event)
 
 
@@ -60,6 +79,14 @@ class CommandFeedback(UITextBox):
         super().__init__('', rect, manager, *args, **kwargs)
 
 
+class LogTextBox(UITextBox):
+    def __init__(self, manager, **kwargs):
+        self.text = ''
+        rect = pygame.Rect((1,1), (198, 600))
+        super().__init__(self.text, rect, manager, **kwargs)
+    
+
+
 class LettersSubmittedPanel(UIPanel):
     pass
 
@@ -68,15 +95,31 @@ class LettersStackPanel(UIPanel):
     pass
 
 
+class WinMessage(UIMessageWindow):
+    def __init__(self, manager, words, submitted_word, *args, **kwargs):
+        rect = pygame.Rect((200, 200), (300, 200))
+        rect.center = (600, 400)
+        to_show = ' '
+        for word in words:
+            if word == submitted_word:
+                to_show += f'<font color=#4DE37C>{word}</font> '
+            else:
+                to_show += word
+        html_message = 'You won!<br>' + to_show
+        super().__init__(rect, html_message, manager, *args, **kwargs)
+
+
 class Gui(Game):
-    def __init__(self, ui_manager, WORDS):
-        super().__init__(WORDS)
+    def __init__(self, ui_manager, level_file):
+        super().__init__(level_file)
         self.ui_manager = ui_manager
-        self.field_panel = FieldPanel(self.field, self.ui_manager)
+        self.field_panel = FieldPanel(self.ui_manager, self.field, self.OPTIONS)
         self.command_input = CommandInput(self.ui_manager)
         self.command_feedback = CommandFeedback(self.ui_manager)
-        # self.letters_submitted_panel = LettersSubmittedPanel()
-        # self.letters_stack_panel = LettersStackPanel()
+        self.logs = LogTextBox(self.ui_manager)
+
+    def show_win_message(self):
+        return WinMessage(self.ui_manager, self.WORDS, ''.join(self.submitted))
 
     def process_event(self, event):
         if event.type == pygame.KEYDOWN:
@@ -84,16 +127,31 @@ class Gui(Game):
                 self.command_input.unfocus()
             elif event.key == pygame.K_SLASH:
                 self.command_input.focus()
-            elif event.key == pygame.K_RETURN:
-                # self.command_feedback.set_text(self.command_input.get_text())
-                # self.field[1][0].contents.c_rotate_clockwise()
+            elif event.key == pygame.K_RETURN and pygame.key.get_mods() & pygame.KMOD_SHIFT:
+                self.show_win_message()
+            elif event.key == pygame.K_RETURN:  # run one command
                 command = self.command_input.get_text()
-                print(f'{int(command[0])} {command[1]}')
-                self.execute_on_group(int(command[0]), command[1])
-        self.update()
-    
+                try:
+                    self.execute_on_group(command)
+                except Warning as e:
+                    print(e)
+                    self.logs.append_html_text('warning<br>')
+                # except Exception as e:
+                #     print(e)
+                #     self.logs.append_html_text('error')
+                #     # self.field = deepcopy(self.field_initial_config)
+                #     self.command_feedback.set_text('')
+
+                self.command_feedback.append_html_text(command + ' ')
+            # TODO: shift + Return = run programm
+        if not self.victory:
+            self.update()
+
     def update(self):
         self.field_panel.update_field(self.field)
+        if self.is_victory():
+            self.show_win_message()
+            self.victory = True
 
 
 def main():
@@ -105,8 +163,8 @@ def main():
     background.fill(pygame.Color('#000000'))
     manager = pygame_gui.UIManager(window_size)
 
-    words = ['hi', 'hike']
-    game = Gui(manager, words)
+    level_file = 'level_files/level2.txt'
+    game = Gui(manager, level_file)
     clock = pygame.time.Clock()
     is_running = True
 
